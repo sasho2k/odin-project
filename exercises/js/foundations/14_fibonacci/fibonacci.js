23182f56857c4b7a2e00d49841fd8186b00e6666
sasho2k <sasho2000@gmail.com>
2026-06-24 08:24:50 -0400

    finished the js foundations problems

--- exercises/js/foundations/14_fibonacci/fibonacci.js
+++ exercises/js/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    
+    n = Number.parseInt(n);
 
+    if (n <= 1) return n;
+
+    let a = 0, b = 1;
+
+    for (let i = 0; i < n - 1; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+        // temp = 1, a = 1, b = 1
+        // temp = 2, a = 1, b = 2
+        // temp = 3, a = 2, b = 3
+        // temp = 5, a = 3, b = 5
+        // temp = 8, a = 5, b = 8
+    }
+    return b;
 };
 
 // Do not edit below this line
--- exercises/js/foundations/16_findTheOldest/findTheOldest.js
+++ exercises/js/foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
+
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return currentAge > oldestAge ? current : oldest;
+    })
+};
+
+/* 
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
