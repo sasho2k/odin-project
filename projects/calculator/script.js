1a0b47eaaffa5589ea50fa73d15788ee440a0f55
sasho2k <sasho2000@gmail.com>
2026-07-04 12:36:40 -0400

    unzip

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,3 +1,6 @@
+// Init variables
+let firstNumber = null, secondNumber = null, operator = "", text = "";
+
 // Calculation functions
 function add(a, b) {
     return a + b;
@@ -15,7 +18,29 @@ function divide(a, b) {
     return a / b;
 }
 
-// 
-function operate() {
-    
+// take two numbers and an operator and return the result
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "x":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
+function pressButton(value) {
+    switch (value) {
+        case "c":
+            text = "";
+            document.getElementById("input-display").value = text;
+            break;
+        default:
+            text += value;
+            document.getElementById("input-display").value = text;
+            break;
+    }
 }
